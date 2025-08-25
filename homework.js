972cac694b94b2d22964838d86f044367063f5c0
berriestrawberrie <brooke.henriksson@outlook.com>
2025-08-25 13:37:08 +0200

    Extra Practice

--- homework.js
+++ homework.js
@@ -1,5 +1,21 @@
 let firstName = "Brooke";
 let favoriteNumber = 8;
-likesCoding = true;
+const likesCoding = true;
 
 console.log(`Hi, my name is ${firstName}, my favorite number is ${favoriteNumber} and it is  ${likesCoding} that I like coding.`);
+
+let birthYear = 1991;
+let currentYear = 2025;
+let age = currentYear - birthYear;
+
+console.log(`I am either ${age} or ${age - 1} years old.`);
+
+let foods = ["pizza", "burger", "tacos" ];
+let student = {
+    firstName: firstName,
+    favoriteNumber: favoriteNumber,
+    likesCoding: likesCoding,
+    favoriteFoods: foods
+};          
+console.log("Student: " ,student);
+console.log(`The student's favorite foods are ${student.favoriteFoods}.`);
