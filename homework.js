bb6f6df473f4eef0c16b15bbce8b1596d4f575ee
berriestrawberrie <brooke.henriksson@outlook.com>
2025-08-25 13:24:25 +0200

    Project Start

---
+++ homework.js
@@ -0,0 +1,5 @@
+let firstName = "Brooke";
+let favoriteNumber = 8;
+likesCoding = true;
+
+console.log(`Hi, my name is ${firstName}, my favorite number is ${favoriteNumber} and it is  ${likesCoding} that I like coding.`);
---
+++ index.js
@@ -0,0 +1,10 @@
+let age = 34;
+let firstName = "Brooke";
+
+const isTeacher = false;
+
+console.log("Name: ", firstName);
+console.log("Age: ", age);  
+console.log("Is a teacher: ", isTeacher);
+
+console.log(`Hi my name is ${firstName}. I am ${age} years old.`);
